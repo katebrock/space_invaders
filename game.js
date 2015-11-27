0076ce79a52602252bc4f9db19d101e040a7716e
Kate Brock <katherine.marshall.brock@gmail.com>
2015-11-27 15:19:44 -0600

    showing on screen

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 ;(function() {
   var Game = function(canvasId) {
-    var canvas = document.getElementById(canvasId)
+    var canvas = document.getElementById(canvasId);
     var screen = canvas.getContext('2d');
-    var gameSize = { x: canvas.width, y: canvas.height};
+    var gameSize = { x: canvas.width, y: canvas.height };
     console.log("hi")
 
     this.bodies = [new Player(this, gameSize)];
@@ -18,10 +18,16 @@
 
   Game.prototype = {
     update: function() {
-      console.log("yoyo")
+      // console.log("yoyo")
+      for (var i = 0; i< this.bodies.length; i++) {
+        this.bodies[i].update();
+      }
     },
     draw: function(screen, gameSize) {
-      for (var i = 0;)
+      screen.clearRect(0, 0, gameSize.x, gameSize.y)
+      for (var i = 0; i < this.bodies.length; i++) {
+            drawRect(screen, this.bodies[i]);
+      }
     }
   };
 
@@ -29,13 +35,42 @@
     this.game = game;
     this.size = { x: 15, y: 15 };
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
   };
 
   Player.prototype = {
     update: function() {
-
+      // console.log("hello")
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x +=2;
+      }
     }
   };
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+  };
+
+  var Keyboarder = function() {
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true;
+    };
+
+    window.onkeyup = function (e) {
+      keyState[e.keycode] = false;
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+     this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
+  }
 
   window.onload = function() {
     new Game("screen");
